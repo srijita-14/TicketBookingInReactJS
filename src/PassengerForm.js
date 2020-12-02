fcd51dab7eabbec54868c1bde55eb979b7b89a33
Srijita Banerjee <srijitabanerjee55@gmail.com>
2020-12-02 20:34:46 +0530

    First Commit

---
+++ src/PassengerForm.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            passenger_name: null,
+            age: null,
+            start_city: null,
+            end_city: null,
+            contact_no: null
+            
+        }
+    }
+
+    passenger_nameChangeHandler = (event) => {
+        this.setState({passenger_name: event.target.value});
+      }
+
+    ageChangeHandler = (event) => {
+        this.setState({age: event.target.value});
+      }
+
+    start_cityChangeHandler = (event) => {
+        this.setState({start_city: event.target.value});
+      }
+
+    end_cityChangeHandler = (event) =>{
+        this.setState({end_city: event.target.value});
+    }
+
+    contact_noChangeHandler = (event) =>{
+        this.setState({contact_no: event.target.value})
+    }
+
+   /* passenger_idChangeHandler = (event) =>{
+        this.setState({passenger_id: event.target.value})
+    }*/
+
+      submitHandler = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8080/booking/passengers', {
+            passenger_name: this.state.passenger_name,
+            age: this.state.age,
+            start_city: this.state.start_city,
+            end_city:  this.state.end_city,
+            contact_no: this.state.contact_no
+        })
+        .then(response => alert("Booking Successful"));
+      }
+
+    render() {
+        return (
+            <form onSubmit={this.submitHandler}>
+              <h1>Passenger Details</h1>
+              <p>Enter your Name:</p>
+              <input
+                type="text"
+                onChange={this.passenger_nameChangeHandler}
+              />
+              <p>Enter your Age:</p>
+              <input
+                type="text"
+                onChange={this.ageChangeHandler}
+              />
+              <p>Enter starting city:</p>
+              <input
+                type="text"
+                onChange={this.start_cityChangeHandler}
+              />
+              <p> Enter destination city: </p>
+              <input
+                type="text"
+                onChange={this.end_cityChangeHandler}
+                />
+              <p> Enter contact number: </p>
+              <input
+                type="text"
+                onChange={this.contact_noChangeHandler}
+               />
+              <p>
+              <input type='submit'/>
+              </p>
+            </form>
+          );
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -14,4 +14,73 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Form from './PassengerForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Train() {
+  return (
+    <h2 className="train">Train</h2>
+  )
+}
+
+
+function Passenger() {
+  return (
+    <h2 className="paseenger">Passenger</h2>
+  )
+}
+
+function Ticket() {
+  return (
+    <h2>Ticket</h2>
+  )
+}
+
+function Default() {
+  return (<Router>
+    <div>
+      
+        <ul>
+          <li>
+            <Link to="/passenger">Passenger</Link>
+          </li>
+          <li>
+            <Link to="/train">Train</Link>
+          </li>
+          <li>
+            <Link to="/ticket">Ticket</Link>
+          </li>
+        </ul>
+      
+
+      <Switch>
+        <Route path="/train">
+          <Train />
+        </Route>
+        <Route path="/ticket">
+          <Ticket />
+        </Route>
+        <Route path="/passenger">
+          <Form/>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+}
+
+ReactDOM.render(
+  <Default />,
+  document.getElementById('root')
+);
